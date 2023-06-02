c7526188e70200ca6c87d71d8a20f418c30a445c
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-02 12:53:01 +0300

    Added a route that serves all orders in database.

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const { orders } = require('./data/orders')
 PORT = 3000
 
+app.use(express.json())
+
+// Frontpage with some text
 app.get('/', (req, res) => {
   res.status(200).send('Backend for IDS collector app.')
+})
+
+// Route that serves all open orders
+app.get('/api/openOrders', (req, res) => {
+  res.json(orders)
 })
 
 app.listen(PORT, () => {
