1da5fa634439568945eec33b7feb16d352979508
Bella <beatrice.raitio@icloud.com>
2023-06-08 15:26:27 +0300

    Quick fix

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+// XML => JSON and JSON => XML
+const { XMLParser, XMLBuilder } = require('fast-xml-parser');
+const parser = new XMLParser
+// Read files
+const { readFileSync } = require('fs')
+const morgan = require('morgan')
+
+// orders from a JSON-file
+//const { orders } = require('./data/orders')
+
+// orders from an XML-file
+const xmlOrders = readFileSync('./data/orders.xml')
+// parse XML order to JSON-object
+let jsonOrders = parser.parse(xmlOrders)
 const orders = require('./routes/orders')
 
 PORT = 3000
@@ -11,6 +25,13 @@ app.use(express.json())
 // Logger middleware
 app.use(morgan('combined'))
 
+// Route that serves all open orders
+app.get('/api/openOrders', (req, res) => {
+  // use this if data is received in XML
+  res.json(jsonOrders)
+  // use this if data is receiced as JSON
+  //res.json(orders)
+})
 app.use('/api', orders)
 
 app.listen(PORT, () => {
