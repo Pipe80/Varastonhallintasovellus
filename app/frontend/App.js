e812abd597433b31bafc5af68d86cf09b1eff09d
Bella <beatrice.raitio@icloud.com>
2023-06-08 14:58:05 +0300

    Folder structure for frontend development environment

--- app/frontend/App.js
+++ app/frontend/App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Login from './components/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World! :)</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login/ >
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
