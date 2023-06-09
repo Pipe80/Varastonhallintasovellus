a02786b9a066163354abcfc8b67fcfc46de6ca2c
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-09 15:34:32 +0300

    Edited getOrders controller for error handling.

--- app/backend/controllers/orderControllers.js
+++ app/backend/controllers/orderControllers.js
@@ -2,19 +2,22 @@
 const { XMLParser, XMLBuilder } = require('fast-xml-parser');
 const parser = new XMLParser
 // Read files
-const { readFileSync } = require('fs')
+const { readFile } = require('fs')
 
 // orders from a JSON-file
 //const { orders } = require('./data/orders')
 
-// orders from an XML-file
-const xmlOrders = readFileSync('./data/orders.xml')
-// parse XML order to JSON-object
-let jsonOrders = parser.parse(xmlOrders)
-
-const getOrders = (req, res) => {
+const getOrders = async (_req, res, next) => {
   // use this if data is received in XML
-  res.json(jsonOrders)
+  readFile('./data/orders.xml', (err, data) => {
+    if (err) {
+      next(err)
+    } else {
+      let jsonOrders = parser.parse(data)
+      res.json(jsonOrders)
+    }
+  })
+
   // use this if data is receiced as JSON
   //res.json(orders)
 }
