ee7bd6a37c03038a3f33ccc439ee89e4dc8a07b1
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-12 13:41:47 +0300

    Added route for creating a new order to database.

--- app/backend/routes/orders.js
+++ app/backend/routes/orders.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const {
   getOrders,
+  createOrder,
 } = require('../controllers/orderControllers.js')
 
 router.get('/openOrders', getOrders)
+router.post('/createOrder', createOrder)
 
 module.exports = router
