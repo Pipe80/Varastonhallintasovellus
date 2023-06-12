cdd4d6696257207a6e4d34dcc94d126657a507c1
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-12 11:38:13 +0300

    Removed .JSON support.

--- app/backend/controllers/orderControllers.js
+++ app/backend/controllers/orderControllers.js
@@ -4,22 +4,16 @@ const parser = new XMLParser
 // Read files
 const { readFile } = require('fs')
 
-// orders from a JSON-file
-//const { orders } = require('./data/orders')
-
 const getOrders = async (_req, res, next) => {
   // use this if data is received in XML
   readFile('./data/orders.xml', (err, data) => {
     if (err) {
       next(err)
     } else {
-      let jsonOrders = parser.parse(data)
+      let jsonOrders = parser.parse(data)      
       res.json(jsonOrders)
     }
   })
-
-  // use this if data is receiced as JSON
-  //res.json(orders)
 }
 
 module.exports = {
