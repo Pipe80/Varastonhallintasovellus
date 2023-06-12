8aef8b3040bb180758dad9f24311ab86f13bfe4f
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-12 14:11:43 +0300

    Started using custom APIerror class.

--- app/backend/middleware/errorHandler.js
+++ app/backend/middleware/errorHandler.js
@@ -1,6 +1,10 @@
+const APIError = require('../errors/apierror')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandler = (err, _req, res, _next) => {
+  if (err instanceof APIError) {
+    return res.status(err.statusCode).json({ message: err.message})
+  }
   if (err.code === 'ENOENT') {
     return res.status(StatusCodes.NOT_FOUND).json( {message: 'No file found in path: ' + err.path })
   } else return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'There was an error' })
