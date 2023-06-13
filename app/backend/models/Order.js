663677f28b8f1bc4dab3327e6ff16994563e8593
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-13 15:24:18 +0300

    Modified to receive data from XML-file

--- app/backend/models/Order.js
+++ app/backend/models/Order.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose")
 
-const itemScema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema({
   name: { type: String, require: true },
-  productID: { type: Number, require: true},
+  productID: { type: String, require: true},
   itemStatus: { type: String, require: true}
 })
 const orderSchema = new mongoose.Schema({
-  orderID: { type: Number, require: true },
-  orderStatus: { type: String, require: true },
-  customer: { type: String, require: true },
-  address: { type: String, require: true },
-  items: [itemScema]
+  order: {
+    orderID: { type: String, require: true },
+    orderStatus: { type: String, require: true },
+    customer: { type: String, require: true },
+    address: { type: String, require: true },
+    items: {item: [itemSchema]}
+  }
 })
 
 const Order = mongoose.model('Order', orderSchema, 'orders')
