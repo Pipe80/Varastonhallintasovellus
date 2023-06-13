349e33501f53fc8fcfb650768732f2495a538f37
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-13 15:31:16 +0300

    Added controllers for XML-routes

--- app/backend/controllers/orderControllers.js
+++ app/backend/controllers/orderControllers.js
@@ -1,12 +1,56 @@
 const Order = require('../models/Order')
+const Order2 = require('../models/Order2')
+const APIError = require('../errors/apierror')
 const { StatusCodes } = require('http-status-codes')
-
 // XML => JSON and JSON => XML
 const { XMLParser, XMLBuilder } = require('fast-xml-parser');
 const parser = new XMLParser
 // Read files
 const { readFile } = require('fs')
 
+// This reads XML from a file and returns it in response
+// This works
+const sendXML = async (req, res, next) => {
+  readFile('./data/order.xml', (err, data) => {
+    let JSONOrder = parser.parse(data)
+    res.json(JSONOrder)
+  })
+  
+}
+
+// This stores one order from XML-file to MongoDB
+// This works
+const createXMLOrderFromFile = async (req, res, next) => {
+  //let JSONOrder
+  readFile('./data/order.xml', (err, data) => {
+    if (err) {
+      next(err)
+    } else {
+      let JSONOrder = parser.parse(data)      
+      console.log(JSONOrder)
+      let order = Order.create(JSONOrder)
+      res.status(StatusCodes.CREATED).json(JSONOrder)
+    }
+  })
+}
+
+// At the moment this takes XML from req.body
+const createXMLOrder = async (req, res, next) => {
+  let XMLOrder = req.body
+  console.log(XMLOrder)
+  let JSONOrder = JSON.stringify(req.body)
+  //JSON.parse(JSONOrder)
+  console.log(JSONOrder)
+  const order = await Order2.create(JSONOrder)
+  //console.log(order)  
+  res.status(StatusCodes.OK).json(JSONOrder)
+}
+
+
+
+
+
+// This reads XML from a file and returns it in responce
 const getOrders = async (_req, res, next) => {
   // use this if data is received in XML
   readFile('./data/orders.xml', (err, data) => {
@@ -19,14 +63,11 @@ const getOrders = async (_req, res, next) => {
   })
 }
 
-const createOrder = async (req, res, _next) => {
+// This takes JSON in req.body and stores it to MongoDB
+// This needs a new model as Order is modified for XML
+const createJSONOrder = async (req, res, _next) => {
   if (Object.keys(req.body).length === 0) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({
-        success: false,
-        message: "No data in body"
-      })
+    throw new APIError('No data in body', StatusCodes.BAD_REQUEST)
   }
   const order = await Order.create(req.body)
   res.status(StatusCodes.CREATED).json({order})
@@ -34,5 +75,8 @@ const createOrder = async (req, res, _next) => {
 
 module.exports = {
   getOrders,
-  createOrder
+  createJSONOrder,
+  sendXML,
+  createXMLOrderFromFile,
+  createXMLOrder
 }
