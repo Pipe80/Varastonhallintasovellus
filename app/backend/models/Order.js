8ba1bf18c7378991109488e8f65ee03e8a5350de
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-16 13:14:28 +0300

    Removed capital letters because Mongo doesn't accept capitals.

--- app/backend/models/Order.js
+++ app/backend/models/Order.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose")
 
 const itemSchema = new mongoose.Schema({
   name: { type: String, require: true },
-  productID: { type: String, require: true},
-  itemStatus: { type: String, require: true}
+  product_id: { type: String, require: true},
+  item_status: { type: String, require: true}
 })
 const orderSchema = new mongoose.Schema({
   order: {
-    orderID: { type: String, require: true },
-    orderStatus: { type: String, require: true },
+    order_id: { type: String, require: true },
+    order_status: { type: String, require: true },
     customer: { type: String, require: true },
     address: { type: String, require: true },
     items: {item: [itemSchema]}
