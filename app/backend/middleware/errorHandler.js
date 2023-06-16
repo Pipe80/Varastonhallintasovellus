8c9719dff52ef9d2cc2fdece673e43a5a2d8897e
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-16 14:39:15 +0300

    Added code to handle mongoose ValidationErrors. Removed unnecessary else.

--- app/backend/middleware/errorHandler.js
+++ app/backend/middleware/errorHandler.js
@@ -2,12 +2,21 @@ const APIError = require('../errors/apierror')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandler = (err, _req, res, _next) => {
+  if (err.name === 'ValidationError') {
+    let errors = {}
+    Object.keys(err.errors).forEach((key) => {
+      errors.key = err.errors[key].message
+    })
+    return res.status(StatusCodes.BAD_REQUEST).send({ success: false, message: err.message})
+  }
   if (err instanceof APIError) {
     return res.status(err.statusCode).json({ message: err.message})
-  }
+  }  
   if (err.code === 'ENOENT') {
     return res.status(StatusCodes.NOT_FOUND).json( {message: 'No file found in path: ' + err.path })
-  } else return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'There was an error' })
+  } 
+  
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'There was an error' })
 }
 
 module.exports = errorHandler
