edbdc26fb3a9147446014e2c40e2c28ce17d0866
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-20 13:51:40 +0300

    Removed unnecessary routes and their controllers. Added route and controller for GET open orders.

--- app/backend/routes/orders.js
+++ app/backend/routes/orders.js
@@ -2,18 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const {
-  getOrders,
-  createJSONOrder,
-  sendXML,
-  createXMLOrderFromFile,
-  createXMLOrder
+  createOrderFromXML,
+  getOpenOrders
 } = require('../controllers/orderControllers.js')
 
-router.get('/openOrders', getOrders)
-router.get('/sendXML', sendXML)
-router.post('/createJSONOrder', createJSONOrder)
-router.post('/XMLOrderFromFile', createXMLOrderFromFile)
-
-router.post('/XMLOrder', createXMLOrder)
+router.post('/createOrderFromXML', createOrderFromXML)
+router.get('/getOpenOrders', getOpenOrders)
 
 module.exports = router
