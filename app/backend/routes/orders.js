5ce85672f275b1d1114019562dd3764a46a3492c
Lassi Iiskola <AC8425@student.jamk.fi>
2023-06-20 14:48:41 +0300

    Created a route for GET all orders.

--- app/backend/routes/orders.js
+++ app/backend/routes/orders.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const {
   createOrderFromXML,
+  getAllOrders,
   getOpenOrders
 } = require('../controllers/orderControllers.js')
 
 router.post('/createOrderFromXML', createOrderFromXML)
+router.get('/getAllOrders', getAllOrders)
 router.get('/getOpenOrders', getOpenOrders)
 
 module.exports = router
