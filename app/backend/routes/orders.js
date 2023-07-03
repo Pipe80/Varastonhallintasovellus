759352a83b0c27ce925e7be097fa47cfd80cc9cb
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-03 14:31:02 +0300

    Created PUT route for order editing.

--- app/backend/routes/orders.js
+++ app/backend/routes/orders.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const {
   createOrderFromXML,
   getAllOrders,
-  getOpenOrders
+  getOpenOrders,
+  editOrder
 } = require('../controllers/orderControllers.js')
 
 router.post('/createOrderFromXML', createOrderFromXML)
 router.get('/getAllOrders', getAllOrders)
 router.get('/getOpenOrders', getOpenOrders)
+router.put('/editOrder/:id', editOrder)
 
 module.exports = router
