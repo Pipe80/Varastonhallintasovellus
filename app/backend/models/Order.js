dedcb412392bc227d322a74eba1539abc3e1cd23
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-04 14:23:47 +0300

    Typos corrected

--- app/backend/models/Order.js
+++ app/backend/models/Order.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
 
 const itemSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  pcs: { type: String, require: true },
-  product_id: { type: String, require: true},
-  item_status: { type: String, require: true}
+  name: { type: String, required: true },
+  pcs: { type: String, required: true },
+  product_id: { type: String, required: true},
+  item_status: { type: String, required: true}
 })
 const orderSchema = new mongoose.Schema({
   order: {
-    order_id: { type: String, require: true },
-    order_status: { type: String, require: true },
-    customer: { type: String, require: true },
-    address: { type: String, require: true },
+    order_id: { type: String, required: true },
+    order_status: { type: String, required: true },
+    customer: { type: String, required: true },
+    address: { type: String, required: true },
     items: {item: [itemSchema]}
   }
 })
