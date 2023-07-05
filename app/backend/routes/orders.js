5ebe3163bb04af012584b282c3d916c6ee62fd24
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-05 13:22:16 +0300

    New route for completed collections. Comments for each route.

--- app/backend/routes/orders.js
+++ app/backend/routes/orders.js
@@ -9,10 +9,15 @@ const {
   collectedOrder,
 } = require('../controllers/orderControllers.js')
 
+// route for receiving an order from webshop
 router.post('/createOrderFromXML', createOrderFromXML)
+// route for getting all orders in database
 router.get('/getAllOrders', getAllOrders)
+// route for getting all open orders in database
 router.get('/getOpenOrders', getOpenOrders)
+// route for editing an order in database
 router.put('/editOrder/:id', editOrder)
+// route for completelly collected orders
 router.put('/collectedOrder/:id', collectedOrder)
 
 module.exports = router
