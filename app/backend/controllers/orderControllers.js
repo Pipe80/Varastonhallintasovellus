92f991078fe0996dd6d44e33745a3000e3d57289
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-05 14:35:56 +0300

    Unused code removed.

--- app/backend/controllers/orderControllers.js
+++ app/backend/controllers/orderControllers.js
@@ -175,64 +175,6 @@ const collectedOrder = async (req, res) => {
   })
 }
 
-// Commented controller down from here were just for testing.
-/* 
-// This reads XML from a file and returns it in response
-// This works
-const sendXML = async (req, res, next) => {
-  readFile('./data/order.xml', (err, data) => {
-    if (err) {
-      next(err)
-    } else {
-      let JSONOrder = parser.parse(data)
-      res.json(JSONOrder)
-    }    
-  })  
-}
-
-// This stores one order from XML-file to MongoDB
-// This works
-const createXMLOrderFromFile = async (req, res, next) => {
-  readFile('./data/order.xml', (err, data) => {
-    if (err) {
-      next(err)
-    } else {
-      let JSONOrder = parser.parse(data)      
-      console.log(JSONOrder)
-      let order = Order.create(JSONOrder)
-      res.status(StatusCodes.CREATED).json(JSONOrder)
-    }
-  })
-}
-
-
-
-// This reads XML from a file and returns it in response
-// This works
-const getOrders = async (_req, res, next) => {  
-  readFile('./data/orders.xml', (err, data) => {
-    if (err) {
-      next(err)
-    } else {
-      let jsonOrders = parser.parse(data)      
-      res.json(jsonOrders)
-    }
-  })
-}
-
-// This takes JSON in req.body and stores it to MongoDB
-// This works
-const createJSONOrder = async (req, res, _next) => {
-  if (Object.keys(req.body).length === 0) {
-    throw new APIError('No data in body', StatusCodes.BAD_REQUEST)
-  }
-  const order = await Order2.create(req.body)
-  res.status(StatusCodes.CREATED).json({order})
-}
-
-*/
-
-
 module.exports = {
   createOrderFromXML,
   getAllOrders,
