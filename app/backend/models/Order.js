bbae163f69165a67ed5f1c5d1acd5fd402218f2e
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-05 13:19:53 +0300

    Updated data structure for more information

--- app/backend/models/Order.js
+++ app/backend/models/Order.js
@@ -12,6 +12,10 @@ const orderSchema = new mongoose.Schema({
     order_status: { type: String, required: true },
     customer: { type: String, required: true },
     address: { type: String, required: true },
+    postal_code: { type: Number, required: true },
+    city: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
     items: { item: [itemSchema] }
   }
 })
