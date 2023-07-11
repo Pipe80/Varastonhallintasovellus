d79506dd375a4a3c9bbeaa2790f842520b16d05c
AD7722 <ad7722@student.jamk.fi>
2023-07-11 10:19:32 +0300

    Upload New File

---
+++ app/frontend/components/Register/register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './styles';
+import { Text, View } from 'react-native';
+
+const Register = ()  => {
+  return (
+    <View style={Styles.container}>
+      <Text>Lorem Ipsum!</Text>
+    </View>
+  );
+};
+
+export default Register;
