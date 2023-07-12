20fa89215d6b660401e9ad40a3fbaaacc408b5a6
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-12 10:01:59 +0300

    Some strange changes in my playground

--- app/frontend/App.js
+++ app/frontend/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import Navigation from './navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen';
 import LoginScreen from './components/LoginScreen';
+import { StyleSheet } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [text, onChangeText] = useState('Terve!')
+
   return (
     //<Navigation/ >
-    <NavigationContainer>
+    <NavigationContainer>      
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer>    
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    marginTop: 40,
+    marginLeft: 20,
+  },
+})
 
 export default App;
