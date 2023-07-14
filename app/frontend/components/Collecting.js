3c125058187dd830810e318f5408c5d9831e10c2
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-14 14:13:15 +0300

    Fetch to get order from backend. List to list items to be collected.

--- app/frontend/components/Collecting.js
+++ app/frontend/components/Collecting.js
@@ -1,43 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Text, TextInput, View, TouchableOpacity, FlatList } from 'react-native';
 import styles from '../styles/BarcodeStyles'
 
 export default function Collecting() {
-  const [items, setItems] = useState({})
-  const orderID = '64a550ad189b5d629f56282b'
+  const [isLoading, setLoading] = useState(false);
+  const [order, setOrder] = useState({})
+
+  const orderID = '64a550e1189b5d629f562835'
   const getItems = () => {
-    fetch('')
+    fetch('http://127.0.0.1:3000/api/getOrderById/' + orderID)
+    .then(res => res.json())
+    .then(json => setOrder(json))
+    .catch(error => console.log(error))
+    .finally(() => setLoading(false))
   }
 
-  const items2 = [
-    {
-      "name": "Nappi",
-      "pcs": 10,
-      "product_id": "1",
-      "item_status": "Not collected",
-      "_id": "64abc08e30b34928714190ae"
-    },
-    {
-      "name": "Lanka",
-      "pcs": 1,
-      "product_id": "2",
-      "item_status": "Not collected",
-      "_id": "64abc08e30b34928714190af"
-    },
-    {
-      "name": "Ohje",
-      "pcs": 1,
-      "product_id": "3",
-      "item_status": "Not collected",
-      "_id": "64abc08e30b34928714190b0"
-    }
-  ]  
-
+  useEffect(() => {
+    setLoading(true);
+    getItems()
+  }, [])
+  
   return (
     <View style={styles.container}>
       <FlatList 
-        data={items2}
-        renderItem={({item}) => <Text>{item.name}, {item.pcs} kappaletta</Text>}
+        data={order.order.items.item}
+        renderItem={({item}) => <Text>tuote: {item.name}, id: {item.product_id}, {item.pcs} kappaletta</Text>}
       />
     </View>
   );
