1c194ac7e5ec9cb07a1c01c846ddd5e4a972df39
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-14 10:51:54 +0300

    Added barcode reading screen, styles and import to App

--- app/frontend/App.js
+++ app/frontend/App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Screens
 import HomeScreen from './components/Home';
 import LoginScreen from './components/Login';
+import Barcode from './components/Barcode';
 
 const Stack = createStackNavigator();
 
