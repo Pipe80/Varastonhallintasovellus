2746f04dd55123ab4f9cfb72f9d7317e5483253d
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-18 10:50:43 +0300

    Fixed problem with asynchronous data fetch.

--- app/frontend/components/Collecting.js
+++ app/frontend/components/Collecting.js
@@ -1,31 +1,52 @@
 import { useState, useEffect } from 'react';
-import { Text, TextInput, View, TouchableOpacity, FlatList } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import styles from '../styles/BarcodeStyles'
 
 export default function Collecting() {
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   const [order, setOrder] = useState({})
 
-  const orderID = '64a550e1189b5d629f562835'
-  const getItems = () => {
-    fetch('http://127.0.0.1:3000/api/getOrderById/' + orderID)
+  // orderID comes from parent component, props?
+  const orderID = '64a550ad189b5d629f56282b'
+  // get IPv4 for your 
+  //    Windows machine:
+  //      - start terminal and type 'ipconfig'  
+  const computerIPv4 = '192.168.100.105'
+  
+  // fetch data asynchronously from backend
+  const getItems = async () => {
+    await fetch('http://' + computerIPv4 + ':3000/api/getOrderById/' + orderID)
     .then(res => res.json())
-    .then(json => setOrder(json))
+    .then(json => setOrder(json))    
     .catch(error => console.log(error))
     .finally(() => setLoading(false))
   }
 
+  // run setLoading() and getItems() on each render
   useEffect(() => {
     setLoading(true);
     getItems()
-  }, [])
+  }, [])  // <-- empty brackets makes useEffect run only once
+
+  // if still loading return 'Ladataan'
+  if (isLoading === true) {
+    return (
+      <View style={styles.container}>
+        <Text>Ladataan</Text>
+      </View>
+    )
+  }
+
+  // if loading is complete return data
+  if (isLoading === false) {
+    return (
+      <View style={styles.container}>
+        <FlatList 
+          data={order.order.items.item}
+          renderItem={({item}) => <Text>tuote: {item.name}, id: {item.product_id}, {item.pcs} kappaletta</Text>}
+        />     
+      </View>
+    );
+  }
   
-  return (
-    <View style={styles.container}>
-      <FlatList 
-        data={order.order.items.item}
-        renderItem={({item}) => <Text>tuote: {item.name}, id: {item.product_id}, {item.pcs} kappaletta</Text>}
-      />
-    </View>
-  );
 }
