c84442e570269b5ffbe28de3919c80fe06ca3adc
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-18 13:08:01 +0300

    Styles for Collecting component.

--- app/frontend/styles/CollectingStyles.js
+++ app/frontend/styles/CollectingStyles.js
@@ -7,31 +7,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#BAE7F7',
   },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    backgroundColor: '#02718D',
+    alignSelf: 'stretch', 
+    height: 74,
+  },
+  backButton: {
+    marginRight: 10,
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
   input: {
     width: 292,
-    height: 52,
+    height: 30,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
-    marginBottom: 10,
-    alignSelf: 'center',
+    paddingVertical: 3,
+    marginTop: 10,    
     backgroundColor: 'white'
-  },  
-  Button: {
-    backgroundColor: '#02718D',
-    paddingHorizontal: 41,
-    paddingVertical: 10,
-    borderRadius: 5,
-    alignSelf: 'center',
-    width: 200, 
-    height: 50, 
-  },
-  ButtonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
+  }
 });
 
 export default styles;
