c230eb60b3081026b01f5c6e52b1672ac0caa42d
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-25 12:40:45 +0300

    Button for navigating to 'Collecting' screen for testign purposes.

--- app/frontend/screens/Orders.js
+++ app/frontend/screens/Orders.js
@@ -4,15 +4,26 @@ import { Flex } from 'native-base';
 // Components
 import Card from '../components/Card/Card';
 
+// I have used 'Button' from native-base because I can't make onPress
+// work with custom 'Button'.
+//import Button from '../components/Button/Button'
+import { Button } from 'native-base';
+
 // Data test
 import data from '../data/orders.json';
 
-const OrdersScreen = () => {
+
+const OrdersScreen = ({ navigation }) => {
+  // orderID is just so that I can test Collecting screen. Lassi :)
+  const orderID = '64a550ad189b5d629f56282b'
   return (
     <Flex>
       {data.orders.map((order) => (
         <Card key={order.id} title={order.title}></Card>
       ))}
+
+      {/* Next line is here just so that I can test Collecting screen. Lassi :)*/}
+      <Button onPress={() => navigation.navigate('Collecting', { orderID: {orderID} })}>Valitse tilaus</Button>
     </Flex>
   );
 };
