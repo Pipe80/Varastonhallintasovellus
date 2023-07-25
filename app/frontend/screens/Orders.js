3f6856402159318348cf49e440195db77e5ab4c4
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-25 13:27:57 +0300

    Start using custom button.

--- app/frontend/screens/Orders.js
+++ app/frontend/screens/Orders.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Flex } from 'native-base';
+import styles from '../styles/CollectingStyles' 
 
 // Components
 import Card from '../components/Card/Card';
 
-// I have used 'Button' from native-base because I can't make onPress
-// work with custom 'Button'.
-//import Button from '../components/Button/Button'
-import { Button } from 'native-base';
+import CustomButton from '../components/Button/Button'
+import { Text } from 'native-base';
 
 // Data test
 import data from '../data/orders.json';
 
 
 const OrdersScreen = ({ navigation }) => {
-  // orderID is just so that I can test Collecting screen. Lassi :)
+  // orderID is just so that I can test Collecting screen. Regards: Lassi :)
   const orderID = '64a550ad189b5d629f56282b'
   return (
     <Flex>
@@ -22,8 +21,10 @@ const OrdersScreen = ({ navigation }) => {
         <Card key={order.id} title={order.title}></Card>
       ))}
 
-      {/* Next line is here just so that I can test Collecting screen. Lassi :)*/}
-      <Button onPress={() => navigation.navigate('Collecting', { orderID: {orderID} })}>Valitse tilaus</Button>
+      {/* Next line is here just so that I can test Collecting screen. Regards: Lassi :)*/}
+      <CustomButton onPress={() => navigation.navigate('Collecting', { orderID: {orderID} })}>
+        <Text style={styles.barcodeReaderButtonText}>Valitse tilaus</Text>
+      </CustomButton>
     </Flex>
   );
 };
