c8a86ac11d74a24a6ed31340824f3c14ecf1a64e
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-27 15:07:10 +0300

    Update order status 'Open' => 'In collection'. Commenting.

--- app/frontend/screens/OrdersLassi.js
+++ app/frontend/screens/OrdersLassi.js
@@ -29,11 +29,13 @@ const OrdersScreen = ({ navigation }) => {
   }
 
   useEffect(() => {
+    // Let's add a listener for focus on this screen to force render
     navigation.addListener('focus', () => {
       setLoading(true)
       getOpenOrders()
     })    
   }, [])
+
 
   if (isLoading === true) {
     return (
@@ -48,13 +50,32 @@ const OrdersScreen = ({ navigation }) => {
     return (
       <ScrollView>
         <Flex>
+          {/* 
+          Map through open orders and create a new Card component
+          for each open order.
+          */}
           {openOrders.map((order) => (          
             <Card 
               key={order._id} 
               title={order.order.customer} 
               id={order._id}>
-              <CustomButton onPress={(id) => {
+              <CustomButton 
+                onPress={async () => {
                   let orderID = order._id
+                  // Change order status to 'In collection'
+                  order.order.order_status = 'In collection'
+                  // Create PUT request
+                  const requestOptions = {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(order)
+                  };
+                  // Make a PUT request to backend to update 
+                  // order status 'Open' => 'In collection'
+                  await fetch('http://' + computerIPv4 + ':3000/api/editOrder/' + orderID, requestOptions)
+                    .then(res => res.json())
+                    
+                  // Navigate to 'Collecting' screen with orderID in props.
                   navigation.navigate('Collecting', { orderID: {orderID} })
                 }}>
                 <Text style={styles.barcodeReaderButtonText}>Valitse</Text>
