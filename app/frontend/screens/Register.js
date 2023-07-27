7e01a54ff66039b37040494a58f6f69fbd1cad3c
Alexander Littler <ad7722@student.jamk.fi>
2023-07-27 15:32:24 +0300

    Register.js koodia muokattu.

--- app/frontend/screens/Register.js
+++ app/frontend/screens/Register.js
@@ -1,50 +1,69 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
-import { styles } from '../styles/RegisterStyles';
+import { View, TextInput, Button, Alert } from 'react-native';
+import styles from '../styles/RegisterStyles';
 
-
-
-const RegisterScreen = (navigation) => {
+const RegisterScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegister = () => {
-    const navigation = {
+    const newUser = {
       username: username,
       email: email,
       password: password
     };
 
-    // Perform registration logic here (e.g., API call to register the user)
-
-    // Assuming successful registration, navigate to the "MainApp" screen
-    navigation.navigate('MainApp');
+    // Make an HTTP POST request to the backend server
+    fetch('YOUR_BACKEND_API_ENDPOINT/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the backend responds with a success status
+        // (You can define what success/failure means in your API)
+        Alert.alert('Registration successful!');
+        navigation.navigate('MainApp'); // Navigate to the main app screen after successful registration
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the registration process
+        Alert.alert('Registration failed. Please try again.');
+        console.error('Error during registration:', error);
+      });
   };
 
+ 
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.input}
+<View style={styles.container}>
+<TextInput
+        style={styles.input}
         placeholder="Username"
         value={username}
         onChangeText={text => setUsername(text)}
       />
-      <TextInput style={styles.input}
+<TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={text => setEmail(text)}
         keyboardType="email-address"
       />
-      <TextInput style={styles.input}
+<TextInput
+        style={styles.input}
         placeholder="Password"
         value={password}
         onChangeText={text => setPassword(text)}
         secureTextEntry
       />
-      <Button title="Register" onPress={handleRegister} />
-      onPress={() => navigation.navigate('Details')}
-    </View>
+<Button title="Register" onPress={handleRegister} />
+</View>
   );
 };
+
+ 
 
 export default RegisterScreen;
