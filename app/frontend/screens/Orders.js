ec0e03a028ae728b02a6d80fdd687acbb41f5995
Lassi Iiskola <AC8425@student.jamk.fi>
2023-07-28 13:12:03 +0300

    Cleanup of unnecessary components.

--- app/frontend/screens/Orders.js
+++ app/frontend/screens/Orders.js
@@ -1,32 +1,101 @@
 import React from 'react';
-import { Flex } from 'native-base';
-import styles from '../styles/CollectingStyles' 
+import { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { Flex, ScrollView, Text, Button } from 'native-base';
+import styles from '../styles/OrderStyles'
+import { Feather } from '@expo/vector-icons';
 
 // Components
 import Card from '../components/Card/Card';
-
 import CustomButton from '../components/Button/Button'
-import { Text } from 'native-base';
-
-// Data test
-import data from '../data/orders.json';
-
 
 const OrdersScreen = ({ navigation }) => {
-  // orderID is just so that I can test Collecting screen. Regards: Lassi :)
-  const orderID = '64a550ad189b5d629f56282b'
-  return (
-    <Flex>
-      {data.orders.map((order) => (
-        <Card key={order.id} title={order.title}></Card>
-      ))}
+  const [isLoading, setLoading] = useState(true);
+  const [openOrders, setOpenOrders] = useState({});
 
-      {/* Next line is here just so that I can test Collecting screen. Regards: Lassi :)*/}
-      <CustomButton onPress={() => navigation.navigate('Collecting', { orderID: {orderID} })}>
-        <Text style={styles.barcodeReaderButtonText}>Valitse tilaus</Text>
-      </CustomButton>
-    </Flex>
-  );
+  // get IPv4 for your Windows machine:
+  //      - start terminal and type 'ipconfig'  
+  const computerIPv4 = '192.168.100.105'
+  
+  // fetch data asynchronously from backend
+  const getOpenOrders = async () => {
+    await fetch('http://' + computerIPv4 + ':3000/api/getOpenOrders/')
+    .then(res => res.json())
+    .then(json => setOpenOrders(json))    
+    .catch(error => console.log(error))
+    .finally(() => setLoading(false))    
+  }
+
+  useEffect(() => {
+    // Let's add a listener for focus on this screen to force render
+    navigation.addListener('focus', () => {
+      setLoading(true)
+      getOpenOrders()
+    })    
+  }, [])
+
+
+  if (isLoading === true) {
+    return (
+      <Flex>
+        <Text>Ladataan</Text>
+      </Flex>
+    )
+  }
+  
+  if (isLoading === false) {    
+    return (
+      <ScrollView>
+        <Flex>
+          <View style={styles.header}>
+            <Button 
+              style={styles.backButton} 
+              onPress={() => navigation.goBack()}
+            >
+              <Feather name="arrow-left" size={24} color="white" />
+            </Button>       
+            <Text style={styles.headerTitle}>Avoimet tilaukset</Text>
+          </View>
+
+          {/* 
+          Map through open orders and create a new Card component
+          for each open order.
+          */}
+          {openOrders.map((order) => (          
+            <Card 
+              key={order._id} 
+              title={order.order.customer} 
+              content={'tilausnumero: ' + order.order.order_id}
+            >
+
+              <CustomButton                
+                onPress={async () => {
+                  let orderID = order._id
+                  // Change order status to 'In collection'
+                  order.order.order_status = 'In collection'
+                  // Create PUT request
+                  const requestOptions = {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(order)
+                  };
+                  // Make a PUT request to backend to update 
+                  // order status 'Open' => 'In collection'
+                  await fetch('http://' + computerIPv4 + ':3000/api/editOrder/' + orderID, requestOptions)
+                    .then(res => res.json())
+                    
+                  // Navigate to 'Collecting' screen with orderID in props.
+                  navigation.navigate('Collecting', { orderID: {orderID} })
+                }}>
+                <Text style={styles.buttonText}>Valitse</Text>
+              </CustomButton>
+            </Card>
+            )
+          )}
+        </Flex>
+      </ScrollView>
+    );
+  }
 };
 
 export default OrdersScreen;
